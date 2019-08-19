444b75c0659e7364d07b1ec299ff67447e31d02c
Amit Amit Saini <n9650911@qut.edu.au>
2019-08-19 16:25:43 +1000

    modified for public view

---
+++ Kowalski Analyses/config.js
@@ -0,0 +1,7 @@
+﻿module.exports = {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+}
+
---
+++ Kowalski Analyses/public/javascripts/main.js
@@ -0,0 +1,329 @@
+﻿var reload;
+
+//reloads the page on given times 
+function loadlive(time) {
+    var button = document.getElementById("toggle");
+    if (button.value == 'on') {
+        reload = setTimeout(() => {
+            window.location.reload();
+        }, time);
+    }
+}
+
+// stops reloading page when the stop button is clicked
+function stoplive(time) {
+    var button = document.getElementById("toggle");
+    if (button.value == "on") {
+        button.value = 'off';
+        button.classList.add('btn-danger');
+        button.classList.remove('btn-success');
+        button.innerHTML = "Live stopped";
+        clearTimeout(reload);
+    }
+    else if (button.value == "off") {
+        button.value = 'on';
+        button.classList.remove('btn-danger');
+        button.classList.add('btn-success');
+        button.innerHTML = "Running Live";
+        reload = setTimeout(() => {
+            window.location.reload();
+        }, time);
+    }
+}
+
+
+// takes the input of the tweet's results array and writes it onto the webpage
+function tweetWriter(resultarray) {
+
+    if (resultarray.length == 0) {
+        document.getElementById('wordcloud').style.display = 'none';
+        document.getElementById('sentiment').style.display = 'none';
+
+        document.getElementById("text").innerHTML = "Sorry no results found matching your Keyword. Search another key word "
+    } else if (resultarray === 0) {
+        document.getElementById('wordcloud').style.display = 'none';
+        document.getElementById('sentiment').style.display = 'none';
+
+    } else {
+        resultarray.forEach(element => {
+            $('#text').append("<p id='tweets'>" + element.tweets + "</p>");
+        });
+    }
+}
+
+
+//configration function of the sentiment liquid ball
+//need to specify how will the sentiment ball will looks
+function liquidFillGaugeDefaultSettings() {
+    return {
+        minValue: 0, // The gauge minimum value.
+        maxValue: 100, // The gauge maximum value.
+        circleThickness: 0.05, // The outer circle thickness as a percentage of it's radius.
+        circleFillGap: 0.05, // The size of the gap between the outer circle and wave circle as a percentage of the outer circles radius.
+        circleColor: "#0D47A1", // The color of the outer circle.
+        waveHeight: 0.08, // The wave height as a percentage of the radius of the wave circle.
+        waveCount: 3, // The number of full waves per width of the wave circle.
+        waveRiseTime: 2000, // The amount of time in milliseconds for the wave to rise from 0 to it's final height.
+        waveAnimateTime: 3000, // The amount of time in milliseconds for a full wave to enter the wave circle.
+        waveRise: true, // Control if the wave should rise from 0 to it's full height, or start at it's full height.
+        waveHeightScaling: true, // Controls wave size scaling at low and high fill percentages. When true, wave height reaches it's maximum at 50% fill, and minimum at 0% and 100% fill. This helps to prevent the wave from making the wave circle from appear totally full or empty when near it's minimum or maximum fill.
+        waveAnimate: true, // Controls if the wave scrolls or is static.
+        waveColor: "#2196F3", // The color of the fill wave.
+        waveOffset: 0, // The amount to initially offset the wave. 0 = no offset. 1 = offset of one full wave.
+        textVertPosition: .5, // The height at which to display the percentage text withing the wave circle. 0 = bottom, 1 = top.
+        textSize: 1, // The relative height of the text to display in the wave circle. 1 = 50%
+        valueCountUp: true, // If true, the displayed value counts up from 0 to it's final value upon loading. If false, the final value is displayed.
+        displayPercent: true, // If true, a % symbol is displayed after the value.
+        textColor: "#045681", // The color of the value text when the wave does not overlap it.
+        waveTextColor: "#A4DBf8" // The color of the value text when the wave overlaps it.
+    };
+}
+
+//function takes the input of the element where to show the ball on the page 
+// with the value and the configration. 
+function loadLiquidFillGauge(elementId, value, config) {
+    document.getElementById('fillgauge').style.display = 'block';
+    if (config == null) config = liquidFillGaugeDefaultSettings();
+    var gauge = d3.select("#" + elementId);
+    var radius = Math.min(parseInt(gauge.style("width")), parseInt(gauge.style("height"))) / 2;
+    var locationX = parseInt(gauge.style("width")) / 2 - radius;
+    var locationY = parseInt(gauge.style("height")) / 2 - radius;
+    var fillPercent = Math.max(config.minValue, Math.min(config.maxValue, value)) / config.maxValue;
+    var waveHeightScale;
+    if (config.waveHeightScaling) {
+        waveHeightScale = d3.scale.linear()
+            .range([0, config.waveHeight, 0])
+            .domain([0, 50, 100]);
+    } else {
+        waveHeightScale = d3.scale.linear()
+            .range([config.waveHeight, config.waveHeight])
+            .domain([0, 100]);
+    }
+
+    var textPixels = (config.textSize * radius / 2);
+    var textFinalValue = parseFloat(value).toFixed(2);
+    var textStartValue = config.valueCountUp ? config.minValue : textFinalValue;
+    var percentText = config.displayPercent ? "%" : "";
+    var circleThickness = config.circleThickness * radius;
+    var circleFillGap = config.circleFillGap * radius;
+    var fillCircleMargin = circleThickness + circleFillGap;
+    var fillCircleRadius = radius - fillCircleMargin;
+    var waveHeight = fillCircleRadius * waveHeightScale(fillPercent * 100);
+
+    var waveLength = fillCircleRadius * 2 / config.waveCount;
+    var waveClipCount = 1 + config.waveCount;
+    var waveClipWidth = waveLength * waveClipCount;
+    // Rounding functions so that the correct number of decimal places is always displayed as the value counts up.
+    var textRounder = function (value) { return Math.round(value); };
+    if (parseFloat(textFinalValue) != parseFloat(textRounder(textFinalValue))) {
+        textRounder = function (value) { return parseFloat(value).toFixed(1); };
+    }
+    if (parseFloat(textFinalValue) != parseFloat(textRounder(textFinalValue))) {
+        textRounder = function (value) { return parseFloat(value).toFixed(2); };
+    }
+    // Data for building the clip wave area.
+    var data = [];
+    for (var i = 0; i <= 40 * waveClipCount; i++) {
+        data.push({ x: i / (40 * waveClipCount), y: (i / (40)) });
+    }
+
+    // Scales for drawing the outer circle.
+    var gaugeCircleX = d3.scale.linear().range([0, 2 * Math.PI]).domain([0, 1]);
+    var gaugeCircleY = d3.scale.linear().range([0, radius]).domain([0, radius]);
+
+    // Scales for controlling the size of the clipping path.
+    var waveScaleX = d3.scale.linear().range([0, waveClipWidth]).domain([0, 1]);
+    var waveScaleY = d3.scale.linear().range([0, waveHeight]).domain([0, 1]);
+
+    // Scales for controlling the position of the clipping path.
+    var waveRiseScale = d3.scale.linear()
+        // The clipping area size is the height of the fill circle + the wave height, so we position the clip wave
+        // such that the it will overlap the fill circle at all when at 0%, and will totally cover the fill
+        // circle at 100%.
+        .range([(fillCircleMargin + fillCircleRadius * 2 + waveHeight), (fillCircleMargin - waveHeight)])
+        .domain([0, 1]);
+    var waveAnimateScale = d3.scale.linear()
+        .range([0, waveClipWidth - fillCircleRadius * 2]) // Push the clip area one full wave then snap back.
+        .domain([0, 1]);
+
+    // Scale for controlling the position of the text within the gauge.
+    var textRiseScaleY = d3.scale.linear()
+        .range([fillCircleMargin + fillCircleRadius * 2, (fillCircleMargin + textPixels * 0.7)])
+        .domain([0, 1]);
+
+    // Center the gauge within the parent SVG.
+    var gaugeGroup = gauge.append("g")
+        .attr('transform', 'translate(' + locationX + ',' + locationY + ')');
+
+    // Draw the outer circle.
+    var gaugeCircleArc = d3.svg.arc()
+        .startAngle(gaugeCircleX(0))
+        .endAngle(gaugeCircleX(1))
+        .outerRadius(gaugeCircleY(radius))
+        .innerRadius(gaugeCircleY(radius - circleThickness));
+    gaugeGroup.append("path")
+        .attr("d", gaugeCircleArc)
+        .style("fill", config.circleColor)
+        .attr('transform', 'translate(' + radius + ',' + radius + ')');
+
+    // Text where the wave does not overlap.
+    var text1 = gaugeGroup.append("text")
+        .text(textRounder(textStartValue) + percentText)
+        .attr("class", "liquidFillGaugeText")
+        .attr("text-anchor", "middle")
+        .attr("font-size", textPixels + "px")
+        .style("fill", config.textColor)
+        .attr('transform', 'translate(' + radius + ',' + textRiseScaleY(config.textVertPosition) + ')');
+
+    // The clipping wave area.
+    var clipArea = d3.svg.area()
+        .x(function (d) { return waveScaleX(d.x); })
+        .y0(function (d) { return waveScaleY(Math.sin(Math.PI * 2 * config.waveOffset * -1 + Math.PI * 2 * (1 - config.waveCount) + d.y * 2 * Math.PI)); })
+        .y1(function (d) { return (fillCircleRadius * 2 + waveHeight); });
+    var waveGroup = gaugeGroup.append("defs")
+        .append("clipPath")
+        .attr("id", "clipWave" + elementId);
+    var wave = waveGroup.append("path")
+        .datum(data)
+        .attr("d", clipArea)
+        .attr("T", 0);
+
+    // The inner circle with the clipping wave attached.
+    var fillCircleGroup = gaugeGroup.append("g")
+        .attr("clip-path", "url(#clipWave" + elementId + ")");
+    fillCircleGroup.append("circle")
+        .attr("cx", radius)
+        .attr("cy", radius)
+        .attr("r", fillCircleRadius)
+        .style("fill", config.waveColor);
+
+    // Text where the wave does overlap.
+    var text2 = fillCircleGroup.append("text")
+        .text(textRounder(textStartValue) + percentText)
+        .attr("class", "liquidFillGaugeText")
+        .attr("text-anchor", "middle")
+        .attr("font-size", textPixels + "px")
+        .style("fill", config.waveTextColor)
+        .attr('transform', 'translate(' + radius + ',' + textRiseScaleY(config.textVertPosition) + ')');
+
+    // Make the value count up.
+    if (config.valueCountUp) {
+        var textTween = function () {
+            var i = d3.interpolate(this.textContent, textFinalValue);
+            return function (t) { this.textContent = textRounder(i(t)) + percentText; }
+        };
+        text1.transition()
+            .duration(config.waveRiseTime)
+            .tween("text", textTween);
+        text2.transition()
+            .duration(config.waveRiseTime)
+            .tween("text", textTween);
+    }
+
+    // Make the wave rise. wave and waveGroup are separate so that horizontal and vertical movement can be controlled independently.
+    var waveGroupXPosition = fillCircleMargin + fillCircleRadius * 2 - waveClipWidth;
+    if (config.waveRise) {
+        waveGroup.attr('transform', 'translate(' + waveGroupXPosition + ',' + waveRiseScale(0) + ')')
+            .transition()
+            .duration(config.waveRiseTime)
+            .attr('transform', 'translate(' + waveGroupXPosition + ',' + waveRiseScale(fillPercent) + ')')
+            .each("start", function () { wave.attr('transform', 'translate(1,0)'); }); // This transform is necessary to get the clip wave positioned correctly when waveRise=true and waveAnimate=false. The wave will not position correctly without this, but it's not clear why this is actually necessary.
+    } else {
+        waveGroup.attr('transform', 'translate(' + waveGroupXPosition + ',' + waveRiseScale(fillPercent) + ')');
+    }
+
+    if (config.waveAnimate) animateWave();
+
+    function animateWave() {
+        wave.attr('transform', 'translate(' + waveAnimateScale(wave.attr('T')) + ',0)');
+        wave.transition()
+            .duration(config.waveAnimateTime * (1 - wave.attr('T')))
+            .ease('linear')
+            .attr('transform', 'translate(' + waveAnimateScale(1) + ',0)')
+            .attr('T', 1)
+            .each('end', function () {
+                wave.attr('T', 0);
+                animateWave(config.waveAnimateTime);
+            });
+    }
+}
+
+
+
+
+// function gets the words from the server and parses it to the wordcloud function. 
+
+function parsewords(words) {
+    var wordsarray = [];
+    if (words.length == 0 || words === 0) {
+
+    } else {
+        words.forEach(element => {
+            for (var i = 0; i < element.length; i++) {
+                wordsarray.push(element[i]);
+
+            }
+
+
+        });
+    }
+
+
+    // configrations of the word cloud. 
+    var myConfig = {
+        type: 'wordcloud',
+        options: {
+            text: wordsarray.toString(),
+
+            minLength: 5,
+            ignore: ['And', 'and', 'about', 'again', 'already', 'are', 'as', 'at', 'before', 'could', 'for', 'great', 'had', 'have', 'how', 'however', 'in', 'is', 'it', 'nothing', 'of', 'on', 'other', 'so', 'shall', 'still', 'that', 'to', 'The', 'the', 'their', 'then', 'there', 'thought', 'until', 'was', 'were', 'what', 'When', 'when', 'which', 'whole', 'with', 'would', 'your', 'https', 'undefined'],
+            maxItems: 100,
+            token: 'word', //word is default. other option is character.
+
+            colorType: 'palette',
+            palette: ['#E91E63', '#2196F3', '#4CAF50', '#FFC107', '#00BCD4', '#673AB7', '#8BC34A'],
+
+            rotate: true,
+            aspect: 'spiral', //'flow-center', 'flow-top', 'spiral',
+
+
+            style: {
+                fontFamily: 'Arial',
+
+                hoverState: {
+                    alpha: 1,
+                    backgroundColor: '#2196F3',
+                    borderRadius: 3,
+                    fontColor: 'white',
+                    textAlpha: 1,
+                },
+                tooltip: {
+                    visible: true,
+                    text: '%text: %hits',
+
+                    alpha: 0.9,
+                    backgroundColor: '#E91E63',
+                    borderColor: 'none',
+                    borderRadius: 3,
+                    fontColor: 'white',
+                    fontFamily: 'Courier',
+                    textAlpha: 1
+                }
+            }
+        }
+    };
+
+    // draws the word cloud based on the data
+    zingchart.render({
+        id: 'myChart',
+        data: myConfig,
+        height: 450,
+        width: '100%'
+    });
+
+
+
+
+
+    return wordsarray
+}
